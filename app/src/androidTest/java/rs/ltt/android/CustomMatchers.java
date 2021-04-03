7a9b066f7c25a4dd4c4ed1957f88673eab5d5a5d
Daniel Gultsch <daniel@gultsch.de>
2021-04-03 18:49:39 +0200

    split Tests into SetupTest and PreexistingMailboxTest

--- app/src/androidTest/java/rs/ltt/android/CustomMatchers.java
+++ app/src/androidTest/java/rs/ltt/android/CustomMatchers.java
@@ -2,7 +2,12 @@ package rs.ltt.android;
 
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.matcher.BoundedMatcher;
+
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.common.base.Preconditions;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
@@ -33,5 +38,25 @@ public class CustomMatchers {
         };
 
 
+    }
+
+    public static Matcher<View> atPosition(final int position, @NonNull final Matcher<View> itemMatcher) {
+        Preconditions.checkNotNull(itemMatcher);
+        return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("has item at position " + position + ": ");
+                itemMatcher.describeTo(description);
+            }
+
+            @Override
+            protected boolean matchesSafely(final RecyclerView view) {
+                final RecyclerView.ViewHolder viewHolder = view.findViewHolderForAdapterPosition(position);
+                if (viewHolder == null) {
+                    return false;
+                }
+                return itemMatcher.matches(viewHolder.itemView);
+            }
+        };
     }
 }
--- app/src/androidTest/java/rs/ltt/android/PreexistingMailboxTest.java
+++ app/src/androidTest/java/rs/ltt/android/PreexistingMailboxTest.java
@@ -1,6 +1,7 @@
 package rs.ltt.android;
 
 import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -13,17 +14,26 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 import okhttp3.mockwebserver.MockWebServer;
+import rs.ltt.android.ui.activity.ComposeActivity;
 import rs.ltt.android.ui.activity.LttrsActivity;
 import rs.ltt.android.ui.activity.SetupActivity;
 import rs.ltt.jmap.client.api.HttpJmapApiClient;
+import rs.ltt.jmap.common.entity.Role;
 import rs.ltt.jmap.mock.server.JmapDispatcher;
 import rs.ltt.jmap.mock.server.MockMailServer;
 
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
 import static androidx.test.espresso.action.ViewActions.swipeDown;
+import static androidx.test.espresso.action.ViewActions.swipeRight;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition;
@@ -36,14 +46,23 @@ import static rs.ltt.android.CustomMatchers.atPosition;
 import static rs.ltt.android.CustomMatchers.withError;
 
 @RunWith(AndroidJUnit4.class)
-public class SetupTest {
+public class PreexistingMailboxTest {
 
     @Rule
     public ActivityScenarioRule<SetupActivity> activityRule = new ActivityScenarioRule<>(SetupActivity.class);
 
 
     private final MockWebServer mockWebServer = new MockWebServer();
-    private final MockMailServer mockMailServer = new MockMailServer(128);
+    private final MockMailServer mockMailServer = new MockMailServer(128) {
+        @Override
+        protected List<MailboxInfo> generateMailboxes() {
+            return Arrays.asList(
+                    new MailboxInfo(UUID.randomUUID().toString(), "Inbox", Role.INBOX),
+                    new MailboxInfo(UUID.randomUUID().toString(), "Archive", null), //no role -> assignment screen
+                    new MailboxInfo(UUID.randomUUID().toString(), "Drafts", Role.SENT) //wrong role -> reassignment screen
+            );
+        }
+    };
     private final OkHttp3IdlingResource okHttp3IdlingResource = OkHttp3IdlingResource.create("OkHttp", HttpJmapApiClient.OK_HTTP_CLIENT);
 
     @Before
@@ -59,75 +78,60 @@ public class SetupTest {
     }
 
     @Test
-    public void setupAndRefresh() throws InterruptedException {
+    public void setupAndSwipePreexistingArchive() throws InterruptedException {
+        onView(withId(R.id.email_address)).perform(typeText(mockMailServer.getUsername()));
+        onView(withId(R.id.email_address)).perform(pressImeActionButton());
+        Thread.sleep(1000);
+        //onView(withId(R.id.next)).perform(click());
+        onView(withId(R.id.url)).perform(typeText(mockWebServer.url(JmapDispatcher.WELL_KNOWN_PATH).toString()));
+        onView(withId(R.id.url)).perform(pressImeActionButton());
+        //onView(withId(R.id.next)).perform(click());
+        Thread.sleep(1000);
+
+        onView(withId(R.id.password)).perform(typeText(JmapDispatcher.PASSWORD));
+        onView(withId(R.id.password)).perform(pressImeActionButton());
+        //onView(withId(R.id.next)).perform(click());
+
+        Thread.sleep(5000);
+
+        intended(hasComponent(LttrsActivity.class.getName()));
+        onView(withId(R.id.thread_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, swipeRight()));
+
+        Thread.sleep(5000);
+
+        //TODO check title
+
+        onView(withText("Confirm")).perform(click());
+
+        Thread.sleep(2000);
+
+        onView(withId(R.id.swipe_to_refresh)).perform(swipeDown());
+    }
+
+    @Test
+    public void setupAndDraft() throws InterruptedException {
         onView(withId(R.id.email_address)).perform(typeText(mockMailServer.getUsername()));
         onView(withId(R.id.next)).perform(click());
-
-        onView(withId(R.id.header)).check(matches(withText("Info required")));
-
         onView(withId(R.id.url)).perform(typeText(mockWebServer.url(JmapDispatcher.WELL_KNOWN_PATH).toString()));
         onView(withId(R.id.next)).perform(click());
 
         onView(withId(R.id.password)).perform(typeText(JmapDispatcher.PASSWORD));
         onView(withId(R.id.next)).perform(click());
 
-        Thread.sleep(3000);
+        Thread.sleep(2000);
 
         intended(hasComponent(LttrsActivity.class.getName()));
 
-        onView(withId(R.id.thread_list))
-                .perform(scrollToPosition(0))
-                .check(matches(atPosition(0, hasDescendant(withText("Mary Smith")))));
+        onView(withId(R.id.compose)).perform(click());
+        intended(hasComponent(ComposeActivity.class.getName()));
 
-        mockMailServer.generateEmailOnTop();
+        onView(withId(R.id.subject)).perform(typeText("Hello word!"));
+        closeSoftKeyboard();
+        pressBack();
 
-        onView(withId(R.id.swipe_to_refresh)).perform(swipeDown());
+        //TODO check title
 
         Thread.sleep(3000);
-
-        onView(withId(R.id.thread_list))
-                .perform(scrollToPosition(0))
-                .check(matches(atPosition(0, hasDescendant(withText("Sandra Anderson")))));
-    }
-
-    @Test
-    public void invalidManuallyEnteredEndpoint() {
-        onView(withId(R.id.email_address)).perform(typeText(mockMailServer.getUsername()));
-        onView(withId(R.id.next)).perform(click());
-        onView(withId(R.id.url)).perform(typeText(mockWebServer.url("not-found").toString()));
-        onView(withId(R.id.next)).perform(click());
-        onView(withId(R.id.url_input_layout)).check(matches(withError(
-                InstrumentationRegistry.getInstrumentation().getTargetContext().getString(R.string.endpoint_not_found)
-        )));
-
-
-    }
-
-    @Test
-    public void invalidEmailAddress() {
-        onView(withId(R.id.email_address)).perform(typeText("incomplete"));
-        onView(withId(R.id.next)).perform(click());
-        onView(withId(R.id.email_address_input_layout)).check(matches(withError(
-                InstrumentationRegistry.getInstrumentation().getTargetContext().getString(R.string.enter_a_valid_email_address)
-        )));
-    }
-
-    @Test
-    public void wrongPassword() {
-        onView(withId(R.id.email_address)).perform(typeText(mockMailServer.getUsername()));
-        onView(withId(R.id.next)).perform(click());
-
-        onView(withId(R.id.url)).perform(typeText(mockWebServer.url(JmapDispatcher.WELL_KNOWN_PATH).toString()));
-
-        onView(withId(R.id.next)).perform(click());
-
-        onView(withId(R.id.password)).perform(typeText("wrong!"));
-
-        onView(withId(R.id.next)).perform(click());
-
-        onView(withId(R.id.password_input_layout)).check(matches(withError(
-                InstrumentationRegistry.getInstrumentation().getTargetContext().getString(R.string.wrong_password)
-        )));
     }
 
     @After
